b856048687deb4fda54cc2b611125a05ddebf5be
dafne <dafnemonozn@gmail.com>
2021-08-12 21:32:02 -0300

    verificaciones

--- js/login.js
+++ js/login.js
@@ -2,5 +2,20 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("bttn").addEventListener("click", function(e){
+ var mail= document.getElementById("inputemail").value;
+ var contra= document.getElementById("inputpassword").value;
+        if ((mail != "")&&(contra != "")){
+            window.location.href="indexx.html";
+        }
 
+     
+    });
+
+   
+
+       
+        
+  
+        
 });
