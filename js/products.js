5706ba40a68edba0883201d9fd1da407a22024ec
dafne <dafnemonozn@gmail.com>
2021-08-20 00:02:50 -0300

    fetch changes

--- js/products.js
+++ js/products.js
@@ -2,5 +2,49 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    let url = "https:japdevdep.github.io/ecommerce-api/product/all.json";
+
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+
+
+
+            for (let i = 0; i < result.length; i++) {
+                let product = result[i]
+                let name = product.name
+                let description = product.description
+                let cost = product.cost
+                let currency = product.currency
+                let imgs = product.imgSrc
+                let soldCount = product.soldCount
+
+                console.log(result);
+
+
+
+                document.getElementById("dafney").innerHTML += `
+<div class="list-group-item list-group-item-action">
+<div class="row">
+    <div class="col-3">
+        <img src="` + imgs + `" alt="` + description + `" class="img-thumbnail">
+    </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">`+ name + `</h4>
+            <small class="text-muted">` + currency + ' ' + cost + `</small>
+        </div>
+    
+        <div class="text-muted"> <h5>` + description + `</h5></div>
+    </div>
+    
+</div>
+</div>
+
+ `
+
+            }
+
+        })
 
 });
