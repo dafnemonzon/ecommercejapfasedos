fa982bad2eb8d85a157e0b11eca8ef745dac553e
dafne <dafnemonozn@gmail.com>
2021-09-30 18:59:22 -0300

    desafiante

--- js/product-info.js
+++ js/product-info.js
@@ -117,14 +117,33 @@ document.addEventListener("DOMContentLoaded", function (e) {
      let imagen1= array[1].imgSrc
 let imagen2 = array[3].imgSrc
      related = `
-        <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100"> <p> <b> PRODUCTOS RELACIONADOS </b> </p>
-               <img class="img-fluid img-thumbnail" src="` + imagen1 + `"  alt="car1">
-              <img class="img-fluid img-thumbnail" src="` + imagen2 + `" alt="car2"> 
-            </div>
-        </div>
-        `
+       
+           
+        
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 
+  </ol> <h3 class="ui-pdp-questions__questions-list__title">Productos Relacionados </h3> <br>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="` + imagen1 + `" class="d-block w-70"  width="500" height=300" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="` + imagen2 + `" class="d-block  w-70" width="500" height="300" alt="...">
+    </div>
+   
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> `
       document.getElementById("relatedproducts").innerHTML =  related;
     }
   }
